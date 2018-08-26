e3f3f8ebec8e629c19c923fe53a7d40aefeacf48
muratkilic1978 <muratkilic1978@gmail.com>
2018-08-26 21:56:32 +0200

    template.css fileupdated and header.php updated with wp_nav_menu theme_location

--- wp-content/themes/learnwptheme/header.php
+++ wp-content/themes/learnwptheme/header.php
@@ -13,14 +13,23 @@
         <section class="top-bar">
             <div class="container">
                 <div class="row">
-                    <div class="social-media-icons col-xl10 col-md-8 col-sm-8 col-6">Social Icons</div>
+                    <div class="social-media-icons col-xl-10 col-md-8 col-sm-8 col-6">Social Icons</div>
                     <div class="search col-xl-2 col-md-4 col-sm-4 col-6 text-right">Search</div> 
                 </div>
             </div>
             
         </section>
         <section class="menu-area">
-            <section class="logo">Logo</section>
-            <nav class="main-menu">Menu</nav>
+            <div class="container">
+                <div class="align">
+                    <div class="row">
+                        <section class="logo col-md-2 col-sm-12 col-12 text-center">Logo</section>
+                        <nav class="main-menu col-md-10 text-right">
+                            <?php wp_nav_menu( array( 'theme_location' => 'primary_menu' ) ); ?>
+                        </nav>   
+                    </div>
+                </div>
+            </div>
+            
         </section>
     </header>
