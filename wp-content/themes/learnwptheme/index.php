9b09c64b42cbcefea3bea3a48dfa6e55fdfc5197
muratkilic1978 <muratkilic1978@gmail.com>
2018-08-26 14:46:18 +0200

    index.php was updated

--- wp-content/themes/learnwptheme/index.php
+++ wp-content/themes/learnwptheme/index.php
@@ -6,5 +6,27 @@
 </head>
 <body>
     <p>This is a paragraph</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+            <section class="map"></section>
+        </main>
+    </div>
+    <footer></footer>
 </body>
 </html>
